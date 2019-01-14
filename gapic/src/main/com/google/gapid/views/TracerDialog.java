27bc3e6dfdab182fe226f369e3bcf68e6346143f
Pascal Muetschard <pmuetschard@google.com>
2019-01-14 13:15:27 -0800

    Basic UI support for loading a Perfetto capture.

--- gapic/src/main/com/google/gapid/views/TracerDialog.java
+++ gapic/src/main/com/google/gapid/views/TracerDialog.java
@@ -105,8 +105,8 @@ public class TracerDialog {
   public static void showOpenTraceDialog(Shell shell, Models models) {
     models.analytics.postInteraction(View.Main, ClientAction.Open);
     FileDialog dialog = new FileDialog(shell, SWT.OPEN);
-    dialog.setFilterNames(new String[] { "Trace Files (*.gfxtrace)", "All Files" });
-    dialog.setFilterExtensions(new String[] { "*.gfxtrace", "*" });
+    dialog.setFilterNames(new String[] { "Trace Files (*.gfxtrace, *.perfetto)", "All Files" });
+    dialog.setFilterExtensions(new String[] { "*.gfxtrace;*.perfetto", "*" });
     dialog.setFilterPath(models.settings.lastOpenDir);
     String result = dialog.open();
     if (result != null) {
@@ -116,9 +116,12 @@ public class TracerDialog {
 
   public static void showSaveTraceDialog(Shell shell, Models models) {
     models.analytics.postInteraction(View.Main, ClientAction.Save);
+    boolean isPerfetto = models.capture.isPerfetto();
     FileDialog dialog = new FileDialog(shell, SWT.SAVE);
-    dialog.setFilterNames(new String[] { "Trace Files (*.gfxtrace)", "All Files" });
-    dialog.setFilterExtensions(new String[] { "*.gfxtrace", "*" });
+    dialog.setFilterNames(new String[] {
+        "Trace Files (" + (isPerfetto ? "*.perfetto" : "*.gfxtrace") + ")", "All Files"
+    });
+    dialog.setFilterExtensions(new String[] { isPerfetto ? "*.perfetto" : "*.gfxtrace", "*" });
     dialog.setFilterPath(models.settings.lastOpenDir);
     String result = dialog.open();
     if (result != null) {
